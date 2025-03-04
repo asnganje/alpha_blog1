0975b112c282804d35b4e7eb275bfa20db2259c0
asnganje <abd.nganje@gmail.com>
2025-03-04 13:17:55 +0300

    Add delete logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,11 +22,16 @@ class ArticlesController < ApplicationController
   end
   def update
     @article = Article.find(params[:id])
-  if @article.update(params.require(:article).permit(:title, :description))
-    flash[:notice] = "Article was successfully updated!"
-    redirect_to @article
-  else
-    render 'edit'
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was successfully updated!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
+  def destroy
+    @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
   end
 end
