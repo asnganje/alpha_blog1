2aaf6f9c82b4ac55432348a1e070220c6d15eb0a
asnganje <abd.nganje@gmail.com>
2025-03-07 11:09:27 +0300

    Add session management logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
   end
   def new
     @user = User.new
@@ -10,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha blog #{@user.username}, successfully signed up"
       redirect_to articles_path
     else
@@ -18,11 +22,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "User updated successfully!"
       redirect_to articles_path
@@ -34,5 +36,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
